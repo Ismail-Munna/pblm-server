1f4949e71be2c1933546479d1ba1d4c6aa6254d0
Ismail Munna <ismailhossainmunna8@gmail.com>
2026-08-02 20:39:31 +0600

    Completely strip ESM for bulletproof CommonJS Vercel Serverless environment

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/server.ts"],
-  format: ["esm"],
-  target: "esnext",
+  format: ["cjs"],
+  target: "node18",
   outDir: "dist",
   clean: true,
   bundle: true,
