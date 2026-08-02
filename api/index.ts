9b6fc34d3a582bc34d5b6de60f0cd13bf293bff5
Ismail Munna <ismailhossainmunna8@gmail.com>
2026-08-02 20:06:29 +0600

    Fix vercel serverless function invocation failed error

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,8 @@
 /// <reference path="../src/types/express.d.ts" />
 import "dotenv/config";
 import app from "../src/app";
-import { prisma } from "../src/lib/prisma";
 
-// Expose the Express app as a serverless function
-export default app;
+// Expose the Express app as a serverless function safely by wrapping it in a handler
+export default function handler(req: any, res: any) {
+  return app(req, res);
+}
